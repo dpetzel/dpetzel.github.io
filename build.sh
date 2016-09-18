a92bdc313b345027dd6964483a03135dd81c2001
Dave Petzel <david.petzel@disney.com>
2016-09-18 16:20:38 -0400

    rebuilding site Sun Sep 18 16:20:38 EDT 2016

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
+#!/bin/sh
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+hugo
 
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin hugo
 git subtree push --prefix public origin master
 
 # Use the following if you get into fast forward nonsense
